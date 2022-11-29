7a966c97a68fe6dacb3d1dd6abf1e804ba5fbb35
Kihyun Ryu <khryu0610@gmail.com>
2022-11-29 10:53:21 +0900

    docs: 폴더 트리 생성

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import HomePage from "./Pages/HomePage/HomePage";
+
+function App() {
+    return (
+        <div>
+            <HomePage></HomePage>
+        </div>
+    );
+}
+export default App;
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../../Components/Card/Card";
+import Cart from "../../Components/Cart/Cart";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Card></Card>
+            <Cart></Cart>
+        </div>
+    );
+}
